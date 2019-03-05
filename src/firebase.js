ded83f8264518e0f3f1553b4ebbd1a6b1e97e960
Charlie Astrada <carlosastrada@gmail.com>
2019-03-05 19:39:44 +0100

    Add base configuration and setup for Firebase/app
    * Attach firebase to window object for development-purposes only (and learning)

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyDNIW6ZEGR6bbfm3xhrEDI1-S73i1gfbzA",
+  authDomain: "think-piece-b9d5b.firebaseapp.com",
+  databaseURL: "https://think-piece-b9d5b.firebaseio.com",
+  projectId: "think-piece-b9d5b",
+  storageBucket: "think-piece-b9d5b.appspot.com",
+  messagingSenderId: "88137199304"
+};
+
+firebase.initializeApp(config);
+
+if (process.env.NODE_ENV === "development") {
+  //TODO: - remove this
+  //This is just to learn and play with the firebase in window object.
+  window.firebase = firebase;
+}
+
+export default firebase;
