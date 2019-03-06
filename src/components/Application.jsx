32fb99a5000efa6e91434eb9da11abe1383a73aa
Charlie Astrada <carlosastrada@gmail.com>
2019-03-07 00:33:41 +0100

    Refactor Posts component to use React's useContext hook to simplify posts rendering pattern.

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,19 +5,11 @@ import Posts from "./Posts";
 import { collectIdsAndDocs } from "../utilities";
 
 class Application extends Component {
-  state = { posts: [], user: null };
+  state = { user: null };
 
-  unsubscribeFromFirestore = null;
   unsubscribeFromAuth = null;
 
   componentDidMount = async () => {
-    this.unsubscribeFromFirestore = firestore
-      .collection("posts")
-      .onSnapshot(snapshot => {
-        const posts = snapshot.docs.map(collectIdsAndDocs);
-        this.setState({ posts });
-      });
-
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       const user = await createUserProfileDocument(userAuth);
       console.log(user);
@@ -30,13 +22,13 @@ class Application extends Component {
   };
 
   render() {
-    const { posts, user } = this.state;
+    const { user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
         <Authentication user={user} />
-        <Posts posts={posts} />
+        <Posts />
       </main>
     );
   }
