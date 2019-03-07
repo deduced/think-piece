06fad9d31ee3ac2d0792490909cfecdb362a6175
Charlie Astrada <carlosastrada@gmail.com>
2019-03-07 11:56:26 +0100

    Add UserProvider to pass User to any component
    Uses the context API

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,33 +1,13 @@
 import React, { Component } from "react";
-import { firestore, auth, createUserProfileDocument } from "../firebase";
 import Authentication from "../components/Authentication";
 import Posts from "./Posts";
-import { collectIdsAndDocs } from "../utilities";
 
 class Application extends Component {
-  state = { user: null };
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount = async () => {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      const user = await createUserProfileDocument(userAuth);
-      console.log(user);
-      this.setState({ user });
-    });
-  };
-
-  componentWillUnmount = () => {
-    this.unsubscribeFromFirestore();
-  };
-
   render() {
-    const { user } = this.state;
-
     return (
       <main className="Application">
         <h1>Think Piece</h1>
-        <Authentication user={user} />
+        <Authentication />
         <Posts />
       </main>
     );
